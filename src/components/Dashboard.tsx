b1480a50014ab04600d7155b6e348c8083084811
Tomas <tomaserobino@gmail.com>
2024-08-22 21:18:46 +0200

    added ForumItems to Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,10 @@
+import ForumItem from "./ForumItem.tsx";
+
 export default function Dashboard() {
   return (
     <div>
-
+      <ForumItem isCategory={true} title={"Announcements"} description={"Announcements regarding our community"} />
+      <ForumItem isCategory={true} title={"General Discussion"} description={"A place to talk about whatever you want"} />
     </div>
   )
 }
