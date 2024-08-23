a6e19898df4928b5d11cab51f606214cd389297d
Tomas <tomaserobino@gmail.com>
2024-08-23 19:36:16 +0200

    some ForumItem styling

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,7 +4,6 @@ export default function Dashboard() {
   return (
     <div>
       <ForumItem isCategory={true} title={"Announcements"} description={"Announcements regarding our community"} />
-      <ForumItem isCategory={true} title={"General Discussion"} description={"A place to talk about whatever you want"} />
     </div>
   )
 }
