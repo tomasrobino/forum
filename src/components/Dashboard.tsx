f0baa43dc07e2b8c5b757c21fa30f901af88923c
Tomas <tomaserobino@gmail.com>
2024-08-25 19:48:31 +0200

    ForumItem styling now done

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,7 +3,7 @@ import ForumItem from "./ForumItem.tsx";
 export default function Dashboard() {
   return (
     <div>
-      <ForumItem title={"Announcements"} description={"Announcements regarding our community dsuio das gdias gdoaydgsadgaso gyds adyugosadga sd gsoadgasdgysad"} />
+      <ForumItem isCategory={true} title={"Announcements"} description={"Announcements regarding our community dsuio das gdias gdoaydgsadgaso gyds adyugosadga sd gsoadgasdgysad"} />
     </div>
   )
 }
