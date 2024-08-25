5b66dc6697d6874809cf450cf9c1a11c934a7cbc
Tomas <tomaserobino@gmail.com>
2024-08-26 00:14:19 +0200

    successful retrieval of categories data in Dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -3,16 +3,15 @@ import {useEffect} from "react";
 
 
 export default function Dashboard() {
+  let categoriesData: unknown[] = [];
   useEffect(() => {
     const url = import.meta.env.VITE_URL;
     //Fetching categories
     fetch(`${url}/forum/categories`)
       .then(data => data.json())
-      .then(data => console.log(data))
+      .then(data => categoriesData = data)
       .catch(error => console.error('Error:', error))
-
   }, [])
-
 
   return (
     <div>
