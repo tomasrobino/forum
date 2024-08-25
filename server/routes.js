f00fbd69ed45b221a8589b5aa7c327a3ec5922ae
Tomas <tomaserobino@gmail.com>
2024-08-26 00:39:48 +0200

    added mock data to server

--- server/routes.js
+++ server/routes.js
@@ -5,26 +5,47 @@ var router = express.Router();
 
 
 //Only for testing purposes
-const categories = [
-  {
-    title: "afdgagdgeragerger",
-    desc: "bqwswqzasaeaweaeaw",
-    lastPost: "cp,o,phjjhopmkj,h",
-    timestamp: "dtr89yrupt98r",
-    user: "erewrew",
-    topics: 1,
-    posts: 2
-  },
-  {
-    title: "freweqwsswd",
-    desc: "gfsfsfwerewr",
-    lastPost: "rewrewreh",
-    timestamp: "idsffewrwerwer",
-    user: "ewrewrewj",
-    topics: 3,
-    posts: 4
-  }
-]
+const categories = [{
+  "title": "ETHYL ALCOHOL",
+  "desc": "In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+  "lastPost": "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+  "timestamp": "8/27/2023",
+  "user": "hfradson0",
+  "topics": 1,
+  "posts": 1
+}, {
+  "title": "Guaifenesin",
+  "desc": "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+  "lastPost": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+  "timestamp": "12/14/2023",
+  "user": "mstanes1",
+  "topics": 2,
+  "posts": 2
+}, {
+  "title": "Alcohol",
+  "desc": "Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna.",
+  "lastPost": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.",
+  "timestamp": "12/16/2023",
+  "user": "fcoda2",
+  "topics": 3,
+  "posts": 3
+}, {
+  "title": "Benzalkonium Chloride",
+  "desc": "Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+  "lastPost": "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+  "timestamp": "12/11/2024",
+  "user": "cdumphy3",
+  "topics": 4,
+  "posts": 4
+}, {
+  "title": "BUSHMASTER VENOM",
+  "desc": "Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.",
+  "lastPost": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+  "timestamp": "12/19/2023",
+  "user": "ashufflebotham4",
+  "topics": 5,
+  "posts": 5
+}]
 
 
 router.get('/forum/categories', function(req, res) {
