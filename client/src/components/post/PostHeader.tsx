5be687941913482e1f124286ef86bef922854d9b
Tomas <tomaserobino@gmail.com>
2024-08-30 17:37:52 +0200

    created PostHeader

---
+++ client/src/components/post/PostHeader.tsx
@@ -0,0 +1,29 @@
+import styles from './PostHeader.module.css'
+import {NavLink} from "react-router-dom";
+
+export function PostHeader(props: {iconColor: string, icon: string, title: string, description: string, topicAmount: number, postAmount: number}) {
+  return (
+    <>
+      <NavLink to={"/"} className={styles.navbar} >Home</NavLink>
+      <div className={styles.header}>
+        <div className={styles.titleNImg}>
+          <div className={styles.iconBox} style={{ backgroundColor: props.iconColor }}>
+            {/* <img className={styles.icon} src={props.icon} alt={props.title}/> */}
+          </div>
+          <p className={styles.title}>{props.title}</p>
+        </div>
+        <p className={styles.description}>{props.description}</p>
+        <div className={styles.quantities}>
+          <div className={styles.box}>
+            <p className={styles.amount}>{props.topicAmount}</p>
+            <p className={styles.amountText}>topics</p>
+          </div>
+          <div className={styles.box}>
+            <p className={styles.amount}>{props.postAmount}</p>
+            <p className={styles.amountText}>posts</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
