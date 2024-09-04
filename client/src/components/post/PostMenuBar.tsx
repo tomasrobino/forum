7ebaf0a56d258c3b1af92fee9c13a96ca9258cdb
Tomas <tomaserobino@gmail.com>
2024-09-04 15:46:37 +0200

    PostMenuBar changes

--- client/src/components/post/PostMenuBar.tsx
+++ client/src/components/post/PostMenuBar.tsx
@@ -3,17 +3,11 @@ import styles from './PostMenuBar.module.css'
 export function PostMenuBar() {
   return (
     <div className={styles.bar}>
-      <div className={styles.barGroup}>
         <div className={styles.buttonDiv}>
           <div className={styles.icon}></div>
-          <button className={styles.button}>All tags</button>
+          <button className={styles.button}>Newest first</button>
         </div>
-        <div className={styles.buttonDiv} style={{ marginLeft: "15px" }}>
-          <div className={styles.icon}></div>
-          <button className={styles.button}>Recently replied</button>
-        </div>
-      </div>
-      <button className={`${styles.button} ${styles.rightButton}`}>Log in to post</button>
+      <button className={`${styles.button} ${styles.rightButton}`}>Log in to reply</button>
     </div>
   );
 }
