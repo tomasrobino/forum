0f49f738c46ecbdf0bc85b5e488ac8863b433ab8
Tomas <tomaserobino@gmail.com>
2024-09-04 16:09:23 +0200

    PostMenuBar select working

--- client/src/components/post/PostMenuBar.tsx
+++ client/src/components/post/PostMenuBar.tsx
@@ -1,11 +1,23 @@
 import styles from './PostMenuBar.module.css'
+import {ChangeEvent, useState} from "react";
+import {useSearchParams} from "react-router-dom";
 
 export function PostMenuBar() {
+  const [selectedValue, setSelectedValue] = useState("newest");
+  const [searchParams, setSearchParams] = useSearchParams();
+  function handleChange(event: ChangeEvent<HTMLSelectElement>) {
+    setSearchParams({"order": event.target.value});
+    setSelectedValue(event.target.value);
+  }
+
   return (
     <div className={styles.bar}>
         <div className={styles.buttonDiv}>
           <div className={styles.icon}></div>
-          <button className={styles.button}>Newest first</button>
+          <select value={selectedValue} onChange={handleChange}>
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+          </select>
         </div>
       <button className={`${styles.button} ${styles.rightButton}`}>Log in to reply</button>
     </div>
