787a6d52259d0f1a96f054fdd85c3417ce5992d0
Tomas <tomaserobino@gmail.com>
2024-10-02 13:09:24 +0200

    sent date to bottom in Reply

--- client/src/components/post/Reply.tsx
+++ client/src/components/post/Reply.tsx
@@ -29,27 +29,27 @@ export function Reply(props: {quote?: { author: string, text: string }, text: st
   }, []);
 
   return (
-    <div className={styles.reply}>
-      <div className={styles.parentPanel}>
-        <div className={styles.userPanel}>
-          <div className={styles.profilePicture}></div>
-          <p className={styles.user}>{user.username}</p>
-          <div className={styles.datapoint}><p className={styles.dataTitle}>joined:</p> <p className={styles.userData}>{user.createdAt}</p></div>
-          <div className={styles.datapoint}><p className={styles.dataTitle}>posts:</p> <p className={styles.userData}>{user.posts}</p></div>
-        </div>
-        <div className={styles.textPanel}>
-          <div className={styles.bar}>
-            <p className={styles.date}>{formattedDate}</p>
+      <div className={styles.reply}>
+        <div className={styles.parentPanel}>
+          <div className={styles.userPanel}>
+            <div className={styles.profilePicture}></div>
+            <p className={styles.user}>{user.username}</p>
+            <div className={styles.datapoint}><p className={styles.dataTitle}>joined:</p> <p
+                className={styles.userData}>{user.createdAt}</p></div>
+            <div className={styles.datapoint}><p className={styles.dataTitle}>posts:</p> <p
+                className={styles.userData}>{user.posts}</p></div>
           </div>
-          {props.quote?
-            <div className={styles.quoteBox}>
-                <p className={styles.quoteAuthor}>{props.quote.author}</p>
-              <p className={styles.quoteText}>{props.quote.text}</p>
-            </div>
-            : null}
-          <p className={styles.text}>{props.text}</p>
+          <div className={styles.textPanel}>
+            {props.quote ?
+                <div className={styles.quoteBox}>
+                  <p className={styles.quoteAuthor}>{props.quote.author}</p>
+                  <p className={styles.quoteText}>{props.quote.text}</p>
+                </div>
+                : null}
+            <p className={styles.text}>{props.text}</p>
+          </div>
         </div>
+        <p className={styles.date}>{formattedDate}</p>
       </div>
-    </div>
   );
 }
