506dc8c3aff979e4e195ba7ed10c8e2d5957d33f
Tomas <tomaserobino@gmail.com>
2024-10-07 11:27:12 +0200

    Register styling, added form to Login

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -5,16 +5,18 @@ export function Login() {
     return (
         <div className={styles.div}>
             <h1>Login</h1>
-            <div className={styles.group}>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" placeholder="Email"/>
-            </div>
-            <div className={styles.group}>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" placeholder="Password"/>
-                <Link className={styles.forgotten} to="/recover-password">Forgot password?</Link>
-            </div>
-            <button>Login</button>
+            <form >
+                <div className={styles.group}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Email"/>
+                </div>
+                <div className={styles.group}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Password"/>
+                    <Link className={styles.forgotten} to="/recover-password">Forgot password?</Link>
+                </div>
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 }
--- client/src/components/login/Register.tsx
+++ client/src/components/login/Register.tsx
@@ -1,5 +1,26 @@
+import styles from "./Login.module.css";
+
 export function Register() {
     return (
-        <></>
+        <div className={styles.div}>
+            <h1>Register</h1>
+            <form>
+                <div className={styles.group}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" placeholder="Username"/>
+                    <p className={styles.hint}>4 to 20 alphanumeric characters.</p>
+                </div>
+                <div className={styles.group}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Email"/>
+                </div>
+                <div className={styles.group}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Password"/>
+                    <p className={styles.hint}>At least 6 characters.</p>
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
     );
 }
