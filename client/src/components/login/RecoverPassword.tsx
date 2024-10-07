ccd9c011d157d90f6e22414c89352f560eaf9ddd
Tomas <tomaserobino@gmail.com>
2024-10-07 11:49:03 +0200

    RecoverPassword styling

--- client/src/components/login/RecoverPassword.tsx
+++ client/src/components/login/RecoverPassword.tsx
@@ -1,5 +1,16 @@
+import styles from "./Login.module.css"
+
 export function RecoverPassword() {
   return (
-    <></>
+    <div className={styles.div}>
+      <p className={styles.desc}>Please enter your account's email address, we will send you an email to help ro</p>
+      <form>
+        <div className={styles.group}>
+          <label htmlFor="email">Email</label>
+          <input required type="email" id="email" name="email" placeholder="Email"/>
+        </div>
+        <button style={{ marginTop: "30px" }} type="submit">Recover Password</button>
+      </form>
+    </div>
   );
 }
