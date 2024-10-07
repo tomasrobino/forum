8624202900481b382e11913b5f5ffb5bcb4fe3d8
Tomas <tomaserobino@gmail.com>
2024-10-07 16:10:50 +0200

    corrected small mistake, deleted unused file

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export function TopBar() {
   return (
     <>
-      <Link to="/">ForumName</Link>
+      <Link to="/" className={styles.link}>ForumName</Link>
       <hr className={styles.bar} />
     </>
   );
