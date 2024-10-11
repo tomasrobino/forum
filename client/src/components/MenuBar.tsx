e74c60af95c5811eec84a168d1e22178238cd8e5
Tomas <tomaserobino@gmail.com>
2024-10-11 12:13:29 +0200

    corrected MenuBar indentation

--- client/src/components/MenuBar.tsx
+++ client/src/components/MenuBar.tsx
@@ -4,32 +4,32 @@ import {useSearchParams} from "react-router-dom";
 import {MenuItem, Select, SelectChangeEvent} from "@mui/material";
 
 export function MenuBar(props: { options: Array<{value: string, name: string}>}) {
-  const [selectedValue, setSelectedValue] = useState(props.options[0].value);
-  const [_searchParams, setSearchParams] = useSearchParams();
-  function handleChange(event: SelectChangeEvent) {
-    setSearchParams({ "select": event.target.value });
-    setSelectedValue(event.target.value);
-  }
+    const [selectedValue, setSelectedValue] = useState(props.options[0].value);
+    const [_searchParams, setSearchParams] = useSearchParams();
+    function handleChange(event: SelectChangeEvent) {
+        setSearchParams({ "select": event.target.value });
+        setSelectedValue(event.target.value);
+    }
 
-  const optionsArray: Array<ReactElement> = [];
-  props.options.forEach(option => {
-    optionsArray.push(<MenuItem key={option.value} value={option.value}>{option.name}</MenuItem>);
-  });
+    const optionsArray: Array<ReactElement> = [];
+    props.options.forEach(option => {
+        optionsArray.push(<MenuItem key={option.value} value={option.value}>{option.name}</MenuItem>);
+    });
 
-  return (
-    <div className={styles.bar}>
-      <Select
-        className={styles.button}
-        value={selectedValue}
-        onChange={handleChange}
-        variant={'outlined'}
-        MenuProps={{
-            disableScrollLock: true,
-        }}
-      >
-        {...optionsArray}
-      </Select>
-      <button className={styles.button}>Log in to reply</button>
-    </div>
-  );
+    return (
+        <div className={styles.bar}>
+            <Select
+                className={styles.button}
+                value={selectedValue}
+                onChange={handleChange}
+                variant={'outlined'}
+                MenuProps={{
+                    disableScrollLock: true,
+                }}
+            >
+                {...optionsArray}
+            </Select>
+            <button className={styles.button}>Log in to reply</button>
+        </div>
+    );
 }
