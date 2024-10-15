acad38b637d3cfaff5d793c76e5a8bb7582c4f38
Tomas <tomaserobino@gmail.com>
2024-10-15 12:58:19 +0200

    WritePost bunch of stuff

--- client/src/components/post/WritePost.tsx
+++ client/src/components/post/WritePost.tsx
@@ -1,15 +1,29 @@
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import {ChangeEvent, useEffect, useState} from "react";
 import {category} from "../../types.ts";
+import styles from "./WriteReply.module.css";
+import {UserPanel} from "./UserPanel.tsx";
+import {user} from "../../types.ts";
+
 
 export function WritePost() {
     const [categories, setCategories] = useState<category[]>([]);
+    const [postError, setPostError] = useState(false);
+    const [text, setText] = useState("");
     const location = useLocation();
     let previousCategory = undefined;
     if (location.state && "category" in location.state && typeof location.state.category === "string") {
         previousCategory = location.state.category;
     }
     const [selected, setSelected] = useState<string>(previousCategory);
+    const [user, setUser] = useState<user>({
+        avatar: Buffer.alloc(0),
+        createdAt: "",
+        posts: 0,
+        topics: 0,
+        username: ""
+    });
+    const navigate = useNavigate();
 
     useEffect(() => {
         const url = import.meta.env.VITE_URL;
@@ -19,7 +33,20 @@ export function WritePost() {
             .then(data => {
                 setCategories(data);
             })
-            .catch(error => console.error('Error:', error))
+            .catch(error => console.error('Error:', error));
+
+        fetch(`${url}/forum/users/${localStorage.getItem("username")}`)
+            .then(data => {
+                if (data.ok) {
+                    return data.json();
+                } else throw new Error("Could not fetch user");
+            })
+            .then(data => {
+                const aux = Buffer.from(data.avatar || "", "binary").toString("base64");
+                delete data.avatar;
+                return setUser({...data, avatar: aux});
+            })
+            .catch(error => console.error('Error:', error));
     }, []);
 
     function handleSelect(e: ChangeEvent<HTMLSelectElement>) {
@@ -31,12 +58,54 @@ export function WritePost() {
         options.push(<option value={category.urlName} key={category.urlName}>{category.title}</option>);
     }
 
+    function handleText(e: ChangeEvent<HTMLTextAreaElement>) {
+        e.preventDefault()
+        setText(e.target.value);
+    }
+
+    function handleCancel() {
+        navigate(-1);
+    }
+
+    async function handlePost() {
+        const url = import.meta.env.VITE_URL;
+        let post = location.pathname;
+        post = post.slice(-24);
+
+        try {
+            await fetch(`${url}/forum/posting/post`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({text: text, author: user.username}),
+            })
+                .then(data => {
+                    if (data.status === 200) {
+                        navigate(-1);
+                    }
+                    setPostError(false);
+                });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <div>
             <p>Write a post</p>
             <select name="categorySelect" value={selected} onChange={handleSelect}>
                 {...options}
             </select>
+            <div className={styles.div}>
+                <UserPanel user={user} />
+                <textarea className={styles.textArea} autoComplete="off" onChange={handleText} value={text}/>
+            </div>
+            <div className={styles.buttonDiv}>
+                <button className={`${styles.button} ${styles.cancelButton}`} onClick={handleCancel}>Cancel</button>
+                <button className={`${styles.button} ${styles.postButton}`} onClick={handlePost}>Post</button>
+            </div>
+            {postError? <p>Error!</p> : null}
         </div>
     );
 }
